6f6fb57a410dbf8d4898abb2f49ff93ab0f95384
Lena <etarnovskaya@gmail.com>
2018-02-13 21:07:09 +0200

    13_02_18

--- trelloSeleniumProject/src/test/java/LoginTrelloTest.java
+++ trelloSeleniumProject/src/test/java/LoginTrelloTest.java
@@ -1,86 +1,38 @@
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import java.util.concurrent.TimeUnit;
+public class LoginTrelloTest extends TestBase {
 
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 
-public class LoginTrelloTest {
-    ChromeDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new  ChromeDriver(); //FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
+
     @Test
     public void LoginTrelloTest() {
-        openSite();
+
         clickLogInButton();
         enterUserName("elena.telran@yahoo.com");
         enterPassword("12345.com");
         confirmLogInButton();
+        logout();
          }
 
     @Test
-    public void LoginTrelloTestNotValidmail() {
-        openSite();
+    public void ALoginTrelloTestNotValidmail() {
+
         clickLogInButton();
         enterUserName("1");
         enterPassword("12345.com");
         confirmLogInButton();
+
     }
 
     @Test
-    public void LoginTrelloemptiFieldTest() {
-        openSite();
+    public void BLoginTrelloemptiFieldTest() {
+        openSite("https://trello.com");
         clickLogInButton();
         enterUserName("");
         enterPassword("");
         confirmLogInButton();
+
     }
 
 
-
-    private void confirmLogInButton() {
-        wd.findElement(By.id("login")).click();
-    }
-
-    private void enterPassword(String pwd) {
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).clear();
-        wd.findElement(By.id("password")).sendKeys(pwd);
-    }
-
-    private void enterUserName(String userName) {
-        wd.findElement(By.id("user")).click();
-        wd.findElement(By.id("user")).clear();
-        wd.findElement(By.id("user")).sendKeys(userName);
-    }
-
-    private void clickLogInButton() {
-        wd.findElement(By.linkText("Log In")).click();
-    }
-
-    private void openSite() {
-        wd.get("https://trello.com");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
